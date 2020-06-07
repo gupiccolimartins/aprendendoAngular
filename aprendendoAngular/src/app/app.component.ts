ad57ec4d655ca6e2f68dee900ccce6cebeb6ceab
gupiccolimartins <gpiccolimartins@gmail.com>
2020-06-07 17:31:33 -0300

    subindo arquivos

--- aprendendoAngular/src/app/app.component.ts
+++ aprendendoAngular/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aprendendoAngular';
+  public exibirConteudo = false;
+  public livros = ['Como morrem os pobres e outros ensaios', 'A revolução dos bichos', '1988'];
+
+  public produto = {
+    tipo: 'livro',
+    titulo: 'Livro',
+    ano: 2000
+  }
+
+  public toggleConteudo() {
+    this.exibirConteudo = !this.exibirConteudo;
+  }
 }
--- aprendendoAngular/src/app/app.module.ts
+++ aprendendoAngular/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OlaMundoComponent } from './ola-mundo/ola-mundo.component';
+import { ContadorComponent } from './contador/contador.component';
+import { LivroComponent } from './livro/livro.component';
+import { RevistaComponent } from './revista/revista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OlaMundoComponent,
+    ContadorComponent,
+    LivroComponent,
+    RevistaComponent
   ],
   imports: [
     BrowserModule
